49bd067c3387f2aa339662f1077113e944fe91a3
Shannon Zylstra <shannon@shannon-VirtualBox.(none)>
2014-06-25 14:54:01 -0400

    init

---
+++ hello.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT''))
+    port = port or 5000
+    app.run(port=port)
