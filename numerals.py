97c8dc318608ac4ea2ce1f4bd8d774ffa554778e
Shannon Zylstra <shannon.rae.zylstra@gmail.com>
2014-06-26 18:04:21 -0400

    Fixed Roman Numeral font

--- numerals.py
+++ numerals.py
@@ -11,7 +11,7 @@ class View(flask.views.MethodView):
 		result = getNumerals(n)
 		display = "%s = %s" % (n, result)
 		flask.flash(display)
-		return flask.render_template('index.html')		
+		return flask.render_template('after.html', number=n, roman=result)
 app.add_url_rule('/', view_func=View.as_view('main'), methods=['GET', 'POST'])
 app.secret_key = os.urandom(24)
 if __name__ == "__main__":
