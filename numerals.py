b949b5859d3547796e8899429d6ad1ab436202a5
Shannon Zylstra <shannon.rae.zylstra@gmail.com>
2014-06-25 22:29:00 -0400

    Make changes to numerals.py

--- numerals.py
+++ numerals.py
@@ -1,14 +1,15 @@
 from flask import Flask
-import flask.views
-import os
+import flask.views, os
+from numeral_converter import getNumerals
+
 app = Flask(__name__)
 class View(flask.views.MethodView):
     def get(self):
 		return flask.render_template('index.html')
     def post(self):
 		n = int(flask.request.form['expression'])
-		result = n**2
-		display = "%s^2 = %s" % (n, result)
+		result = getNumerals(n)
+		display = "%s = %s" % (n, result)
 		flask.flash(display)
 		return self.get()		
 app.add_url_rule('/', view_func=View.as_view('main'), methods=['GET', 'POST'])
