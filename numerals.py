0a07c3e24e293a368fa5c4c114b24c5f273fdf03
Shannon Zylstra <shannon.rae.zylstra@gmail.com>
2014-06-26 19:04:08 -0400

    Make after.html extend index.html

--- numerals.py
+++ numerals.py
@@ -12,6 +12,7 @@ class View(flask.views.MethodView):
 		display = "%s = %s" % (n, result)
 		flask.flash(display)
 		return flask.render_template('after.html', number=n, roman=result)
+		
 app.add_url_rule('/', view_func=View.as_view('main'), methods=['GET', 'POST'])
 app.secret_key = os.urandom(24)
 if __name__ == "__main__":
