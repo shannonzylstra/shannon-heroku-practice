74e07abeb03f03f2ac9d15b990216282e75f5f45
Shannon Zylstra <shannon.rae.zylstra@gmail.com>
2014-06-26 19:06:56 -0400

    Removed flashing

--- numerals.py
+++ numerals.py
@@ -9,10 +9,8 @@ class View(flask.views.MethodView):
     def post(self):
 		n = int(flask.request.form['expression'])
 		result = getNumerals(n)
-		display = "%s = %s" % (n, result)
-		flask.flash(display)
 		return flask.render_template('after.html', number=n, roman=result)
-		
+
 app.add_url_rule('/', view_func=View.as_view('main'), methods=['GET', 'POST'])
 app.secret_key = os.urandom(24)
 if __name__ == "__main__":
