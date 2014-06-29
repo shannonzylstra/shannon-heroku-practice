1e4d9cfe5f6bbd8c31dbdf26150e5085496aea9f
Shannon Zylstra <shannon.rae.zylstra@gmail.com>
2014-06-29 14:39:15 -0400

    Use decorators instead of View

--- numerals.py
+++ numerals.py
@@ -1,17 +1,19 @@
-from flask import Flask
-import flask.views, os
+from flask import Flask, request, render_template
+import os
 from numeral_converter import getNumerals
 
 app = Flask(__name__)
-class View(flask.views.MethodView):
-    def get(self):
-		return flask.render_template('index.html', title="Roman Numeral Converter")
-    def post(self):
-		n = int(flask.request.form['expression'])
-		result = getNumerals(n)
-		return flask.render_template('after.html', number=n, roman=result)
 
-app.add_url_rule('/', view_func=View.as_view('main'), methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
+def convert():
+    if request.method == 'POST':
+        n = int(flask.request.form['expression'])
+        result = getNumerals(n)
+        return render_template('after.html', title="Roman Numeral Converter", number=n, roman=result)
+
+    return render_template('index.html', title="Roman Numeral Converter")
+    
 app.secret_key = os.urandom(24)
+
 if __name__ == "__main__":
     app.run()
